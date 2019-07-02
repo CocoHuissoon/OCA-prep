b7264bdc2d4d5d1ff8df1f123ada3073112670e3
Coco <chuissoon@hotmail.com>
2019-07-02 20:42:21 +0200

    first

---
+++ src/first/StringPrep.java
@@ -0,0 +1,12 @@
+package first;
+
+public class StringPrep {
+
+    public static void main (String [] args) {
+        int x = 0;
+        while (x != 4) {
+            System.out.println(x);
+            x++;
+        }
+    }
+}
